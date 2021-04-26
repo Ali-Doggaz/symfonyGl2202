2209c61fc035dae832f71b33d15d5500e78e7d80
Ali-Doggaz <charfedine611@gmail.com>
2021-04-26 15:29:51 +0100

    complete the TODO function

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -32,7 +32,7 @@ class TodoController extends AbstractController
     }
 
     /**
-     * @Route("/add/{name}/{content<\d+>}", name="addTodo")
+     * @Route("/add/{name}/{content}", name="addTodo")
      */
     public function addTodo($name, $content, SessionInterface $session) {
 
@@ -52,6 +52,59 @@ class TodoController extends AbstractController
                 $todos[$name] = $content;
                 $session->set('todos', $todos);
                 $this->addFlash('success', "Le todo $name a été ajouté avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+
+    /**
+     * @Route("/modify/{name}/{content}", name="modifyToDo")
+     */
+    public function modifyToDo($name, $content, SessionInterface $session) {
+
+        // Vérifier que ma session contient le tableau de todo
+        if (!$session->has('todos')) {
+            //ko => messsage erreur + redirection
+            $this->addFlash('error', "La liste des todos n'est pas encore initialisée");
+        } else {
+            //ok
+            // Je vérifie si le todo existe
+            $todos = $session->get('todos');
+            if (!isset($todos[$name])) {
+                //ko => messsage erreur + redirection
+                $this->addFlash('error', "Le todo $name n'existe pas");
+            } else {
+                //ok => j ajoute et je redirige avec message succès
+                $todos[$name] = $content;
+                $session->set('todos', $todos);
+                $this->addFlash('success', "Le todo $name a été modifié avec succès");
+            }
+        }
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/delete/{name}", name="deleteFromToDo")
+     */
+    public function deleteFromToDo($name, SessionInterface $session) {
+
+        // Vérifier que ma session contient le tableau de todo
+        if (!$session->has('todos')) {
+            //ko => messsage erreur + redirection
+            $this->addFlash('error', "La liste des todos n'est pas encore initialisée");
+        } else {
+            //ok
+            // Je vérifie si le todo existe
+            $todos = $session->get('todos');
+            if (!isset($todos[$name])) {
+                //ko => messsage erreur + redirection
+                $this->addFlash('error', "Le todo $name n'existe pas");
+            } else {
+                //ok => je supprime le todo
+                unset($todos[$name]);
+                $session->set('todos', $todos);
+                $this->addFlash('success', "Le todo $name a été supprimé avec succès");
             }
         }
         return $this->redirectToRoute('todo');
